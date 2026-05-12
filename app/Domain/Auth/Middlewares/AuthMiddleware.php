f78b571d50e8d88faa6a7b101d47d7a72f975dad
khaled-moussa <khaledmoussaeid@gmail.com>
2026-05-12 03:27:02 +0300

    refactor: Remove UserSeeder and implement AdminSeeder for admin user creation

--- app/Domain/Auth/Middlewares/AuthMiddleware.php
+++ app/Domain/Auth/Middlewares/AuthMiddleware.php
@@ -14,7 +14,6 @@ class AuthMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        info(1);
         // Redirect guests to login
         if (! Auth::check()) {
             return redirect()->route('auth.login');
